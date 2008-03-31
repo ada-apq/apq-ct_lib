7043a6e4e7af50f682a70e6fcf371f2156373eff
Adele Helena Ribeiro <adele.helena@gmail.com>
2008-03-31 16:58:09 +0000

    sybase code is formatted.
    git-svn-id: http://dev.adaworks.net/svn/trunk/apq-sybase@303 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/apq-sybase.adb
+++ src/apq-sybase.adb
@@ -1,24 +1,34 @@
--- $Id: apq-sybase.adb,v 1.28 2004/10/05 00:48:30 wwg Exp $
--- Copyright (c) 2003, Warren W. Gay VE3WWG
---
--- Licensed under the ACL (Ada Community License)
--- or
--- GNU Public License 2 (GPL2)
--- 
---     This program is free software; you can redistribute it and/or modify
---     it under the terms of the GNU General Public License as published by
---     the Free Software Foundation; either version 2 of the License, or
---     (at your option) any later version.
--- 
---     This program is distributed in the hope that it will be useful,
---     but WITHOUT ANY WARRANTY; without even the implied warranty of
---     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---     GNU General Public License for more details.
--- 
---     You should have received a copy of the GNU General Public License
---     along with this program; if not, write to the Free Software
---     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---
+------------------------------------------------------------------------------
+--                                                                          --
+--                          APQ DATABASE BINDINGS                           --
+--                                                                          --
+--                              A P Q - SYBASE				    --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--         Copyright (C) 2002-2007, Warren W. Gay VE3WWG                    --
+--         Copyright (C) 2007-2008, Ydea Desenv. de Softwares Ltda          --
+--                                                                          --
+--                                                                          --
+-- APQ is free software;  you can  redistribute it  and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  APQ is distributed in the hope that it will be useful, but WITH-  --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with APQ;  see file COPYING.  If not, write  --
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+------------------------------------------------------------------------------
+
 
 with Ada.Exceptions;
 with Ada.Text_IO;
@@ -626,7 +636,7 @@ package body APQ.Sybase is
 		function Trim(S : String) return String_Ptr is
 		begin
 			if S(S'First) = ' ' then
-				return new String'(S(S'First+1..S'Length));
+				return new String'(S(S'Range));
 			else
 				return new String'(S);
 			end if;
